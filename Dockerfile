af50a4ef75a71316a145eb0037b35d0a2ec92d9e
bishalthapa2055 <bishalthapa2055@gmail.com>
2024-11-05 09:05:28 +0545

    docker build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 # Use an official Node.js image as the base
-FROM node:18-alpine
+FROM ubuntu
+
+RUN apt-get update
+RUN apt-get install -y curl
+RUN curl -sL https://deb.nodesource.com/setup_18.x | bash -
+RUN apt-get update
+RUN apt-get upgrade -y
+RUN apt-get install -y nodejs
 
 # Set the working directory in the container
 WORKDIR /app
--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,14 @@ const start = async () => {
   const server = require("http").createServer(app);
   try {
     const mongodb = "mongodb://localhost:27017/dockers";
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(mongodb).then((res) => {
-      console.log("Connected to mongoDb sucessfully");
-    });
+    // mongoose.set("strictQuery", true);
+    // await mongoose.connect(mongodb).then((res) => {
+    //   console.log("Connected to mongoDb sucessfully");
+    // });
   } catch (e) {
     console.log("ERROR :", e);
   }
-  const port = 9090;
+  const port =process.env.PORT ||  9090;
   server.listen(port, () => {
     console.log(
       `server sttarted at port: http://${ip.address()}:${port}`
